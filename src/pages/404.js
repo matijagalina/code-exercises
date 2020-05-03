485fbb41a7595b483ac776720cd38eefca98b1fd
Matija <matijagalina@gmail.com>
2020-05-03 21:02:50 +0200

    404

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import HeroHeader from "../components/heroHeader"
+
+const Page404 = () => {
+
+  return (
+    <Layout>
+      <HeroHeader/>
+      <h2>Error</h2>
+    </Layout>
+  )
+}
+
+export default Page404
